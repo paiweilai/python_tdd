f79e5906cfe482a1729581e10f8463370b606852
Pai-Wei Lai <paiwei.lai@gmail.com>
2016-04-08 23:32:25 -0700

    bowling kata finish

--- bowling/game.py
+++ bowling/game.py
@@ -1,2 +1,39 @@
 class Game(object):
-    pass
+
+    def __init__(self):
+        self._rolls = [0] * 21
+        self._current_roll = 0
+
+    def roll(self, pins):
+        self._rolls[self._current_roll] = pins
+        self._current_roll += 1
+
+    def is_spare(self, ball_index):
+        return self._rolls[ball_index] + self._rolls[ball_index + 1] == 10
+
+    def is_strike(self, ball_index):
+        return self._rolls[ball_index] == 10
+
+    def get_spare_bonus(self, ball_index):
+        return self._rolls[ball_index + 2]
+
+    def get_strike_bonus(self, ball_index):
+        return self._rolls[ball_index + 1] + self._rolls[ball_index + 2]
+
+    def score(self):
+        s = 0
+        ball_index = 0
+
+        for _ in range(10):
+
+            if self.is_strike(ball_index):
+                s += 10 + self.get_strike_bonus(ball_index)
+                ball_index += 1
+            elif self.is_spare(ball_index):
+                s += 10 + self.get_spare_bonus(ball_index)
+                ball_index += 2
+            else:
+                s += self._rolls[ball_index] + self._rolls[ball_index + 1]
+                ball_index += 2
+
+        return s
--- bowling/test.py
+++ bowling/test.py
@@ -5,8 +5,44 @@ from game import Game
 
 class BowlingGameTest(unittest.TestCase):
 
-    def test_create_game(self):
-        Game()
+    def setUp(self):
+        self.g = Game()
+
+    def roll_many(self, n, pins):
+        for i in range(n):
+            self.g.roll(pins)
+
+    def roll_spare(self):
+        self.g.roll(5)
+        self.g.roll(5)
+
+    def roll_strike(self):
+        self.g.roll(10)
+
+    def test_gutter_game(self):
+        self.roll_many(20, 0)
+        assert self.g.score() == 0
+
+    def test_all_ones(self):
+        self.roll_many(20, 1)
+        assert self.g.score() == 20
+
+    def test_one_spare(self):
+        self.roll_spare()
+        self.g.roll(3)
+        self.roll_many(17, 0)
+        assert self.g.score() == 16
+
+    def test_one_strike(self):
+        self.roll_strike()
+        self.g.roll(3)
+        self.g.roll(4)
+        self.roll_many(16, 0)
+        assert self.g.score() == 24
+
+    def test_perfect_game(self):
+        self.roll_many(12, 10)
+        assert self.g.score() == 300
 
 
 if __name__ == '__main__':
